592a4cb31f381eee869281d611cd1c46b1c31f23
talagi <agnieszkata1@o2.pl>
2018-09-07 14:19:11 +0200

    Cpp

---
+++ cpp/szablon.cpp
@@ -0,0 +1,15 @@
+/*
+ * szablon.cpp
+ */
+
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	int a; // deklaracja zmiennej
+    a = 0; // inicjacja zmiennej
+    std::cout << a;
+	return 0;
+}
+
