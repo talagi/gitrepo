a2b51d90ecfa969eea3b9eb4d9dd008384c9d0bb
talagi <agnieszkata1@o2.pl>
2018-09-17 08:44:24 +0200

    Tabele

---
+++ SQL-baza/magazyn/magazyn.sql
@@ -0,0 +1,25 @@
+DROP TABLE IF EXISTS Customers;
+DROP TABLE IF EXISTS Subscribtion;
+DROP TABLE IF EXISTS Orders;
+
+CREATE TABLE Customers (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT,
+	address TEXT
+);
+
+CREATE TABLE Orders (
+    order_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    customer_id INTEGER,
+    subscribtion_id INTEGER,
+    purchase_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(id),
+    FOREIGN KEY (subscribtion_id) REFERENCES Subscribtion(id)
+);
+
+CREATE TABLE Subscribtion (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    description TEXT,
+    price_per_month DECIMAL,
+    subscription_length TEXT
+);
