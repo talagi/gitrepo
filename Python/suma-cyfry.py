0ddddb537a575832bd19a3477df879029e3de7ea
talagi <agnieszkata1@o2.pl>
2018-10-03 10:34:14 +0200

    Algorytmy

---
+++ Python/suma-cyfry.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  suma-cyfry.py
+#  
+# Napisz program, który sumuje cyfry podanej liczby (minimum dwucyfrowej)
+# i wyœwietla ich sumê w terminalu.
+#
+
+def sumuj_cyfry1(liczba):
+    suma = 0
+    while liczba > 0:
+        suma += liczba % 10
+        liczba = int(liczba / 10)
+    return suma
+    
+def sumuj_cyfry2(liczba):
+    suma = 0
+    while liczba > 0:
+        suma += liczba % 10
+        liczba = int(liczba / 10)
+    return suma
+
+
+def main(args):
+    
+    liczba = input("Podaj liczbê 2-cyfrow¹: ")
+    liczba = int(liczba)
+    
+    while liczba < 10:
+        liczba = input("Podaj liczbê 2-cyfrow¹: ")
+        liczba = int(liczba)
+    
+    suma = 0 
+    while liczba > 0:
+        suma += liczba % 10
+        liczba = int(liczba / 10)
+        
+    
+    print("Suma: ", suma)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
