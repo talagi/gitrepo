1d90d7cadbe7fa15bb0989c620252438c200d6b1
talagi <agnieszkata1@o2.pl>
2018-10-10 19:38:58 +0200

    Praca domowa

--- Python/liczby23.py
+++ Python/liczby23.py
@@ -2,33 +2,44 @@
 # -*- coding: utf-8 -*-
 #
 #  liczby23.py
-#  
-#  
+#
+#
 def liczby2():
     """
     Funkcja drukuje wszystkie liczby dwucyfrowe, w których cyfry nie
-    powtarzają się. Funkcja zwraca ich liczbę. 
+    powtarzają się. Funkcja zwraca ich liczbę.
     Wykluczone liczby: 11, 22, 33 itd.
     """
     ile = 0 # licznik
     for i in range(1, 10):
         for j in range(0, 10):
             if i != j:
-                print("{}{} ".format(i, j), end='') 
+                print("{}{} ".format(i, j), end='')
                 ile = ile + 1
-    return ile 
+    return ile
 
-    
+
 def liczby3():
     """
     Funkcja drukuje wszystkie liczby trzycyfrowe, w których cyfry nie
-    powtarzają się. Funkcja zwraca ich liczbę. 
+    powtarzają się. Funkcja zwraca ich liczbę.
     Wykluczone liczby: 111, 222, 333 itd.
     """
+    ile = 0
+
+    for i in range(1, 10):
+        for j in range(0, 10):
+            for k in range(0, 10):
+                if i != j or j != k or k != i:
+                    print("{}{}{} ".format(i, j,k), end='')
+                    ile = ile + 1
+    print()
+    return ile
+
 
 
 def main(args):
-    print("Liczb 2-cyfrowych:", liczby2())
+    print("Liczb 3-cyfrowych:", liczby3())
     return 0
 
 if __name__ == '__main__':
