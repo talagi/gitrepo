4e4d5f2bd3f982cd3a15da26260beef14bb5115d
talagi <agnieszkata1@o2.pl>
2018-10-23 19:50:43 +0200

    Euklides

--- cpp/Euklides.cpp
+++ cpp/Euklides.cpp
@@ -1,20 +1,32 @@
 /*
- * Euklides.cpp
- * 
+ * euklides.cpp
  */
 
 
 #include <iostream>
-
 using namespace std;
+int nwd_1(int a, int b) {
+    while(a != b) {
+        if (a > b){
+            a = a - b;
+        }
+        else {
+            b = b - a;
+        }
+    }
+    return(a);
+}
+
 
 int main(int argc, char **argv)
 {
-	 int a, b;
-     cout << "Podaj 2 liczby: ";
-     cin >> a >> b;
-     int nwd = nwd1(a, b);
-     cout << "NWD(" << a << "," << b << ") = " << nwd << endl;
-	return 0;
-}
+    int b = 0;
+    int a = 0;
+    cout << "Podaj 2 liczby: ";
+    cin >> a >> b;
 
+    int nwd = nwd_1(a,b);
+        cout << "NWD ("<< a <<","<< b <<") = " << nwd << endl;
+
+    return 0;
+}
