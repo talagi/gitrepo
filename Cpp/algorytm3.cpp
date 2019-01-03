cbbcfb6713c920d9da5ee8a141c40438765dd35d
talagi <agnieszkata1@o2.pl>
2019-01-03 08:43:54 +0100

    Algorytm

---
+++ Cpp/algorytm3.cpp
@@ -0,0 +1,24 @@
+/*
+ * algorytm3.cpp
+ * 
+ */
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	float stopien, radian;
+    stopien = 0.0;
+    
+    do {
+        radian = stopien * M_PI / 180;
+        cout << "cos(" << stopien << ") = " << cos(radian) << endl;
+        stopien += 10.0;
+    } while (stopien <= 90.0);
+    
+	return 0;
+}
+
---
+++ Cpp/algorytm4.cpp
@@ -0,0 +1,17 @@
+/*
+ * algorytm4.cpp
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+void znajdz()
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
+
