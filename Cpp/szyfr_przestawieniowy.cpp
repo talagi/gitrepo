0bd6093a09a9325b64110c372096ad40394b15a7
talagi <agnieszkata1@o2.pl>
2019-03-04 07:14:08 +0100

    Cały tydzień

--- Cpp/szyfr_przestawieniowy.cpp
+++ Cpp/szyfr_przestawieniowy.cpp
@@ -10,25 +10,8 @@ void deszyfruj(char tekst[], int klucz) {
 }
 
 void szyfruj(char tb[], int klucz){
-    klucz = klucz % 26;
-    int kod = 0;
-    int i = 0;
-    while (tb[i] != '\0') {
-        kod = (int)tb[i];
-        if (tb[i] == ' ') {
-            ;
-        } else if (kod < 91) {
-            kod += klucz;
-            if (kod > 90) kod -=26;
-        } else {
-            kod += klucz;
-            if (kod > 122) kod -= 25;
-        }
-        cout << (char)kod;
-        tb[i] = (char)kod;
-        i++;
-    }
-    cout << endl;
+    int ile = strlen(tb);
+    
 }
 
 int main(int argc, char **argv)
